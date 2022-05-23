878ca9c04ff42eb18403196ee02b43370ec526ae
hmcs03 <83166592+hmcs03@users.noreply.github.com>
2022-05-23 15:58:44 +0530

    Add files via upload

---
+++ Version of Python and Pandas.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+from platform import python_version
+print(python_version())
+
+
+# In[4]:
+
+
+import pandas
+
+
+# In[5]:
+
+
+import pandas as pd
+print(pd.__version__)
+
+
+# In[ ]:
+
+
+
+
